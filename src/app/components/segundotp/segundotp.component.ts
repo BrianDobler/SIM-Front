72f79a0c581e46d2b601cb626d5180814094d019
ginomassei <ginomassei@hotmail.com>
2021-08-23 14:48:39 -0300

    Add file method

--- src/app/components/segundotp/segundotp.component.ts
+++ src/app/components/segundotp/segundotp.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SegundotpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
+  
+  selectedFile: any = null;
 
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+    
+    const table = <HTMLTableElement> document.getElementById("randomTable");
+
+    this.selectedFile.forEach( element => {
+      let row = table.insertRow();
+      
+    });
+    console.log(this.selectedFile);
+
+  }
+
+  onUpload() {
+    const fd = new FormData();
+    fd.append(
+      'file', 
+      this.selectedFile, 
+      this.selectedFile.name
+      )
+    console.log(fd)
+    this.http.post('http://localhost:5000/api/submitRandomSequence', fd)
+    .subscribe(res => {
+      console.log(res);
+    })
+  }
 }
