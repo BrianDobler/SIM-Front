f020ff28fb6da9cf87bf86076df6c53fac2de7d1
Gaston Roch <gaston.roch1998@gmail.com>
2021-08-23 00:49:41 -0300

    pag principal terminada

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
-  Titulo = 'Trabajos Prácticos Simulación 2021 UTN - FRC Ingeniería en Sistemas de Información'
   constructor() { }
 
   ngOnInit(): void {
