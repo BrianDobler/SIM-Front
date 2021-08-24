396d7e3929d0b92b5c6a5928884cc6284bb4f18f
ginomassei <ginomassei@hotmail.com>
2021-08-23 23:23:53 -0300

    Second component

--- src/app/components/segundotp/segundotp.component.ts
+++ src/app/components/segundotp/segundotp.component.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -11,41 +12,14 @@ export class SegundotpComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-  }
-  
-  selectedFile: any = null;
-
-  onFileSelected(event: any) {
-    this.selectedFile = event.target.files[0];
     
-    var reader = new FileReader();
-    reader.readAsText(this.selectedFile, "UTF-8");
-    reader.onload = e => {
-      const result = reader.result;
-      const lines = result.split('\n');
-
-      const table = <HTMLTableElement> document.getElementById("randomTable");
-
-      // all.forEach((element: string) => {
-      //   let row = table.insertRow();
-      //   let value = row.insertCell(1);
-  
-      //   value.innerHTML = element;
-      // });
-    } 
   }
 
-  onUpload() {
-    const fd = new FormData();
-    fd.append(
-      'file', 
-      this.selectedFile, 
-      this.selectedFile.name
-      )
-    console.log(fd)
-    this.http.post('http://localhost:5000/api/submitRandomSequence', fd)
-    .subscribe(res => {
-      console.log(res);
-    })
+  taxis() {
+    console.log("taxis")
+  }
+ 
+  france() {
+    console.log("france")
   }
 }
