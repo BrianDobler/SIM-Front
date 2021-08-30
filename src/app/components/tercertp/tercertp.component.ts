b49a27f3d5ae0ccc04a605bba8a0587f66c55f7c
ginomassei <ginomassei@hotmail.com>
2021-08-30 20:44:30 -0300

    Funciona la conexión con la API

--- src/app/components/tercertp/tercertp.component.ts
+++ src/app/components/tercertp/tercertp.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-tercertp',
@@ -8,35 +9,98 @@ import { HttpClient } from '@angular/common/http';
 })
 export class TercertpComponent implements OnInit {
 
-  tiposDistribuciones:string | undefined ;
+  tiposDistribuciones: string = '';
+  baseUrl = 'http://localhost:8080/api/';
+  url = '?numberOfSamples=';
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private formBuilder: FormBuilder
+    ) {}
 
   ngOnInit(): void {
   }
 
-  seleccion(value:string){
-    console.log(value);
+  seleccion(value: string) {
     this.tiposDistribuciones = value;
-
   }
 
-  baseUrl = 'http://localhost:8080/api/';
+  uniformDistributionForm = this.formBuilder.group({
+    a: "",
+    b: "",
+    numberOfSamples: ""
+  })
+
+  exponentialDistributionForm = this.formBuilder.group({
+    lambda: "",
+    numberOfSamples: ""
+  })
   
+  poissonDistributionForm = this.formBuilder.group({
+    lambda: "",
+    numberOfSamples: ""
+  })
+
+  normalDistributionForm = this.formBuilder.group({
+    mu: "",
+    sigma: "",
+    numberOfSamples: ""
+  })
+  
+  onSubmit(): void {
+    switch (this.tiposDistribuciones) {
+      case 'normal-distribution':
+
+        console.log(this.normalDistributionForm.value);
+        this.url = 
+        'normal-distribution' + this.url + 
+        this.normalDistributionForm.value['numberOfSamples'] + 
+        '&mu=' + this.normalDistributionForm.value['mu'] +
+        '&sigma=' + this.normalDistributionForm.value['sigma'];
+        
+        break;
+      
+      case 'exponential-distribution':
+        console.log(this.exponentialDistributionForm.value);
+
+        this.url = 
+        'exponential-distribution' + this.url + 
+        this.exponentialDistributionForm.value['numberOfSamples'] +
+        '&lambda=' + this.exponentialDistributionForm.value['lambda'];
+
+        break;
+
+      case 'poisson-distribution':
+        console.log(this.poissonDistributionForm.value);
+
+        this.url = 
+        'poisson-distribution' + this.url +
+        this.poissonDistributionForm.value['numberOfSamples'] +
+        '&lambda=' + this.exponentialDistributionForm.value['lambda'];
+
+        break;
+      
+      case 'uniform-distribution':
+        console.log(this.uniformDistributionForm.value);
+
+        this.url = 
+        'uniform-distribution' + this.url +
+        this.uniformDistributionForm.value['numberOfSamples'] +
+        '&a=' + this.uniformDistributionForm.value['a'] +
+        '&b=' + this.uniformDistributionForm.value['b'];
+        break;
+    }
+
+    this.request();
+    this.url = ''
+  }
 
   requestData: any;
-  request() {
-     this.http.get("http://localhost:8080/api/poisson-distribution?lambda=4&numberOfSamples=500").subscribe(data => {
-      this.requestData = data;
-
-      console.log(this.requestData)
-    })
+  request(): void {
+    console.log(this.baseUrl + this.url)
+    this.http.get(this.baseUrl + this.url)
+      .subscribe(data => {
+        this.requestData = data;
+      })
   }
-
-  distributions = [
-    'Uniforme',
-    'Poisson',
-    'Normal',
-    'Exponencial'
-  ]
 }
