ad70b0e72cff695fff08f112c7b54f7303e60e9d
ginomassei <ginomassei@hotmail.com>
2021-09-01 20:26:08 -0300

    Heroku

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
-const ngApp = express();
+const app = express();
 
-ngApp.use(express.static('.dist/angular-forms-validation'));
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/angular-app-heroku'));
 
-ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/angular-forms-validation/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+);
 
-ngApp.listen(process.env.PORT || 8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
