ab9d5c2c536b8907a947424a3b4a1b2630add912
ginomassei <ginomassei@hotmail.com>
2021-09-01 20:59:49 -0300

    Testing

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 //Install express server
-import express, { static } from 'express';
+const express = require('express');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(static('./dist/TPSIM/'));
+app.use(express.static('./dist/TPSIM/'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/TPSIM/'}),
 );
 
 // Start the app by listening on the default Heroku port
-// eslint-disable-next-line no-undef
 app.listen(process.env.PORT || 8080);
