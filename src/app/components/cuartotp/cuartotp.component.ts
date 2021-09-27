2c302ce3bac036faf7c1839f8869d361e744c5f8
Brian Dobler <brianahueldobler@gmail.com>
2021-09-27 20:40:45 -0300

    Se agrega

--- src/app/components/cuartotp/cuartotp.component.ts
+++ src/app/components/cuartotp/cuartotp.component.ts
@@ -11,5 +11,25 @@ export class CuartotpComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
-}
+  actividades=[{
+  Nombre: "Actividad 1",
+  Distribucion: "Uniforme",
+  Presedencias: "Sin Presedencias", 
+  Variable: "T1"},{
+  Nombre: "Activdad 2",
+  Distribucion: "Uniforme",
+  Presedencias: "Sin Presedencias", 
+  Variable: "T1"},{
+  Nombre: "Actividad 3",
+  Distribucion: "Exponencial", 
+  Presedencias: "Sin Presedencias",
+  Variable: "T1"},{
+  Nombre: "Activdad 4",
+  Distribucion: "Uniforme", 
+  Presedencias: "Solo Actividad 1",
+  Variable: "T4"},{
+  Nombre: "Actividad 5",
+  Distribucion: "Exponencial",
+  Presedencias: "Solo Actividad 2 y 4",
+  Variable: "T5"},
+  ]}
