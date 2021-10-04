ec8b3b92aa755e7b4c1fcc2cc4d6dce2c1bc4437
ginomassei <ginomassei@hotmail.com>
2021-10-04 12:39:15 -0300

    Styled

--- src/app/components/tercertp/tercertp.component.spec.ts
+++ src/app/components/tercertp/tercertp.component.spec.ts
@@ -3,23 +3,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TercertpComponent } from './tercertp.component';
 
 describe('TercertpComponent', () => {
-  let component: TercertpComponent;
-  let fixture: ComponentFixture<TercertpComponent>;
+    let component: TercertpComponent;
+    let fixture: ComponentFixture<TercertpComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TercertpComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [TercertpComponent]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TercertpComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TercertpComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
