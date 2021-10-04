ea6ed9241e2c8688462f9bdabde729e8e2044860
ginomassei <ginomassei@hotmail.com>
2021-10-04 11:35:45 -0300

    Styled file

--- src/app/components/cuartotp/cuartotp.component.spec.ts
+++ src/app/components/cuartotp/cuartotp.component.spec.ts
@@ -3,23 +3,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CuartotpComponent } from './cuartotp.component';
 
 describe('CuartotpComponent', () => {
-  let component: CuartotpComponent;
-  let fixture: ComponentFixture<CuartotpComponent>;
+    let component: CuartotpComponent;
+    let fixture: ComponentFixture<CuartotpComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CuartotpComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [CuartotpComponent]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CuartotpComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CuartotpComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
