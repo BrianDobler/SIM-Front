d6510ca2dce786481689459b18597e76044dfd1b
ginomassei <ginomassei@hotmail.com>
2021-10-04 23:15:05 -0300

    Service working

--- src/app/components/cuartotp/cuartotp.service.ts
+++ src/app/components/cuartotp/cuartotp.service.ts
@@ -2,9 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { MontecarloBody } from './helpers/MontecarloBody';
+import { MontecarloBody } from './models/Montecarlo.model';
 
-@Injectable({ providedIn:'root' })
+@Injectable({ providedIn: 'root' })
 export class ComponentService {
     baseUrl: string = environment.url;
 
@@ -13,7 +13,7 @@ export class ComponentService {
     setSimulation = (requestBody: MontecarloBody): Observable<any> => {
         const headers = { 'content-type': 'application/json' };
         const body = JSON.stringify(requestBody);
-        console.log(body);
-        return this.http.post(this.baseUrl, body, { headers: headers });
+
+        return this.http.post(this.baseUrl + 'montecarlo/simulate', body, { headers: headers });
     }
 }
