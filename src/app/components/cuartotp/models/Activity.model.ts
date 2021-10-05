4ab4a1e82607dacf7c96541dd8436c8e12bcdb39
ginomassei <ginomassei@hotmail.com>
2021-10-05 11:04:22 -0300

    Modified the activity model. Now it takes the distribution name too.

--- src/app/components/cuartotp/models/Activity.model.ts
+++ src/app/components/cuartotp/models/Activity.model.ts
@@ -2,10 +2,12 @@ export class ActivityModel {
     // Model of an activity.
     // This is the data needed to simulate the activity on the backend.
     name: string;
+    distributionName: string;
     distribution: any
 
-    constructor (name: string, distribution: any) {
+    constructor (name: string, distributionName: string, distribution: any) {
         this.name = name;
+        this.distributionName = distributionName;
         this.distribution = distribution;
     }
 }
