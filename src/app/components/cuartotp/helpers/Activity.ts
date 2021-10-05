ae799472b170a5f5531ea45b250c7d740a6e8dbb
ginomassei <ginomassei@hotmail.com>
2021-10-04 23:14:47 -0300

    Now, setDistributionForm takes optional parameters

--- src/app/components/cuartotp/helpers/Activity.ts
+++ src/app/components/cuartotp/helpers/Activity.ts
@@ -16,30 +16,31 @@ export class Activity {
         if (valueB !== undefined) {
             this.valueB = valueB;
         }
-        this.setDistributionForm();
+        this.setDistributionForm(this.valueA, this.valueB);
     };
 
     selectDistribution = (value: string): void => {
         // Select the distribution to use.
         this.distributionType = value;
+        this.setDistributionForm();
     };
 
-    setDistributionForm = (): void => {
+    setDistributionForm = (valueA?: number, valueB?: number): void => {
         // Depends on the type of distribution that is selected. 
         // Set certain distribution form.
         if (this.distributionType === 'uniform') {
             this.distributionForm =  new FormGroup({
-                A: new FormControl(this.valueA, [Validators.required]),
-                B: new FormControl(this.valueB, [Validators.required])
+                A: new FormControl((valueA !==  null) ? valueA : '', [Validators.required]),
+                B: new FormControl((valueB !==  null) ? valueB : '', [Validators.required])
             });   
         } else if (this.distributionType === 'normal') {
             this.distributionForm =  new FormGroup({
-                mu: new FormControl(this.valueA, [Validators.required, Validators.min(0)]),
-                sigma: new FormControl(this.valueB, [Validators.required, Validators.min(0)])
+                mu: new FormControl((valueA !==  null) ? valueA : '', [Validators.required, Validators.min(0)]),
+                sigma: new FormControl((valueB !==  null) ? valueB : '', [Validators.required, Validators.min(0)])
             });
         } else if (this.distributionType === 'exponential') {
             this.distributionForm =  new FormGroup({
-                lambda: new FormControl(this.valueA, [Validators.required, Validators.min(0)])
+                lambda: new FormControl((valueA !==  null) ? valueA : '', [Validators.required, Validators.min(0)])
             });
         }
     };
